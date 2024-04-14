ac64d529fca73fe4481e3823621dfa10639d5e03
Viateur <akimanaviateur94@gmail.com>
2024-04-14 04:18:42 +0200

    updating models

--- app.js
+++ app.js
@@ -11,13 +11,6 @@ const port = process.env.PORT || 3000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
-.then(() => console.log('Connected to MongoDB'))
-.catch((err) => console.error('Error connecting to MongoDB:', err));
-
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
--- models/User.js
+++ models/User.js
@@ -1,11 +1,53 @@
 const mongoose = require("mongoose");
 
-// User model
-const userSChema =  mongoose.Schema({
+
+const User = mongoose.model('User', {
   name: String,
   email: String,
   password: String,
+  paymentMethod: { type: mongoose.Schema.Types.ObjectId, ref: 'PaymentMethod' }
+});
+const Appointment = mongoose.model('Appointment', {
+  date: Date,
+  time: String,
+  provider: String,
+  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }
+});
+const PaymentMethod = mongoose.model('PaymentMethod', {
+  type: String,
+  last4: String,
+  expMonth: Number,
+  expYear: Number,
+  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }
+});
+const Ambulance = mongoose.model('Ambulance', {
+  licensePlate: String,
+  make: String,
+  model: String,
+  location: {
+    type: {
+      type: String,
+      enum: ['Point'],
+      required: true
+    },
+    coordinates: {
+      type: [Number],
+      required: true
+    }
+  }
 });
 
-const user = mongoose.model("User", userSChema);
-module.exports = user;
+// Driver model
+const Driver = mongoose.model('Driver', {
+  name: String,
+  license: String,
+  ambulance: { type: mongoose.Schema.Types.ObjectId, ref: 'Ambulance' }
+});
+
+module.exports = {
+  User,
+  Appointment,
+  PaymentMethod,
+  Ambulance,
+  Driver
+};
