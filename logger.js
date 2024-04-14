5fabfc9a5969a631945e4bb9b913fbc3ed78d482
Viateur <akimanaviateur94@gmail.com>
2024-04-14 04:32:15 +0200

    embeding winston for log events

--- logger.js
+++ logger.js
@@ -1,16 +1,16 @@
 const {createLogger,combine,transports} = require("winston")
 
-const logger = winston.createLogger({
+const logger = createLogger({
     level: 'info',
     format: winston.format.combine(
-      winston.format.timestamp(),
-      winston.format.printf(({ level, message, timestamp }) => {
+      format.timestamp(),
+      format.printf(({ level, message, timestamp }) => {
         return `[${timestamp}] ${level}: ${message}`;
       })
     ),
     transports: [
-      new winston.transports.Console(),
-      new winston.transports.File({ filename: 'server.log' })
+      new transports.Console(),
+      new transports.File({ filename: 'server.log' })
     ]
 })
 
