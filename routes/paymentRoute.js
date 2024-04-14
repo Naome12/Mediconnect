a0b90cf61b3b9639682f67a2cf32bb3938b623fd
Viateur <akimanaviateur94@gmail.com>
2024-04-14 03:34:01 +0200

    dealing with payment

--- routes/paymentRoute.js
+++ routes/paymentRoute.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const paymentController = require('../controllers/paymentController');
+const paymentController = require('../controllers/paymentControllers');
 const authMiddleware = require('../middleware/authMiddleware');
 
 router.use(authMiddleware);
