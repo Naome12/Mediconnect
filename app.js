b9c82aaed695d649315fac359016d4fa0f99a509
Viateur <akimanaviateur94@gmail.com>
2024-04-15 20:37:58 +0200

    updated app.js

--- app.js
+++ app.js
@@ -1,21 +1,25 @@
 const express = require('express');
 const app = express();
-const connectDB = require('./utils/db'); 
-const logger = require('./utils/logger'); 
+const connectDB = require('./database/connection'); 
+const logger = require('./utils/logger');
 const authRoutes = require('./routes/authRoutes');
 const appointmentsRoutes = require('./routes/appointmentsRoutes');
+const ambulanceRoutes = require('./routes/ambulanceRoutes'); 
+const driverRoutes = require('./routes/driverRoutes'); 
 
-// Middleware
+// Middlewares
 app.use(express.json());
 
-// Connect to MongoDB
+// Connection to MongoDB
 connectDB();
 
-//her are registered routes
+// Routes
 app.use('/auth', authRoutes);
 app.use('/api', appointmentsRoutes);
+app.use('/api', ambulanceRoutes);
+app.use('/api', driverRoutes);
 
-// Error handling
+// Error handling 
 app.use((err, req, res, next) => {
   logger.error('An error occurred:', err);
   res.status(500).json({ error: 'Internal server error' });
